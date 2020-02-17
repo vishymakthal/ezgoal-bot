693abfeb3cfb0ab5ab815c25483802c9c725cd0f
Vishy Makthal <vmakthal@cisco.com>
2020-02-17 16:32:36 -0500

    Updated main file to use better logging and UCL Round of 16 teams

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
    "os"
    "regexp"
    "time"
+   log "github.com/sirupsen/logrus"
 )
 
 const (
@@ -22,7 +23,8 @@ type goalBot struct {
 }
 
 func sendWebExMessage(title string, contentLink string, commentsLink string){
-   msg := fmt.Sprintf(`{"roomId": "%v", "markdown": "%v | [Link](%v) | [Comments / Mirror Links](http://reddit.com%v)"}`,roomID, title, contentLink, commentsLink)
+   log.Info("Posting to WebEx Room")
+   msg := fmt.Sprintf(`{"roomId": "%v", "markdown": "####%v\n| [Link](%v) | [Comments / Mirror Links](http://reddit.com%v)"}`,roomID, title, contentLink, commentsLink)
    fmt.Printf(msg)
    var jsonStr = []byte(msg)
    req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(jsonStr))
@@ -38,25 +40,36 @@ func sendWebExMessage(title string, contentLink string, commentsLink string){
 }
 
 func (r *goalBot) Post(p *reddit.Post) error {
-
    if b,err := regexp.MatchString(`\[[0-9]+\]|[0-9]{1,2}'`,p.Title); b && (err==nil) {
-      <-time.After(5 * time.Second)
-      fmt.Printf("Title: %s | URL: %s | Permalink: %s\n", p.Title, p.URL, p.Permalink)
-      sendWebExMessage(p.Title, p.URL, p.Permalink)
+       if b, err := regexp.MatchString(`Dortmund|Valencia|Bayern|Liverpool|Tottenham|Chelsea|Barcelona|Juventus|Real Madrid`,p.Title); b && (err==nil){
+	   log.Info("Found a goal to send")
+	   <-time.After(10 * time.Second)
+           fmt.Printf("Title: %s | URL: %s | Permalink: %s\n", p.Title, p.URL, p.Permalink)
+           sendWebExMessage(p.Title, p.URL, p.Permalink)
+      }
    }
    return nil
 }
 
 func main() {
+
+   log.Info("Starting bot")
    if bot, err := reddit.NewBotFromAgentFile("creds.txt", 0); err != nil {
-      fmt.Println("Failed to create bot handle: ", err)
+      log.WithFields(log.Fields{
+	      "error" : err,
+      }).Fatal("Failed to create bot handle")
    } else {
       cfg := graw.Config{Subreddits: []string{"soccer"}}
       handler := &goalBot{bot: bot}
       if _, wait, err := graw.Run(handler, bot, cfg); err != nil {
          fmt.Println("Failed to start graw run: ", err)
+	 log.WithFields(log.Fields{
+	      "error" : err,
+           }).Fatal("failed to start graw run")
       } else {
-         fmt.Println("graw run failed: ", wait())
+	log.WithFields(log.Fields{
+	      "error" : wait(),
+      }).Fatal("graw run failed")
       }
    }
 }
